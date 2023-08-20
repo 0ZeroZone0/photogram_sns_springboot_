a5a8409ca9a99ad05e6b3114785cd953f1754862
0Zone <00zero0zone00@gmail.com>
2023-08-20 22:35:17 +0900

    스프링부트 포토그램 인증 UserDetailsService 이해

--- photogram/src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ photogram/src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.util.Script;
 import com.cos.photogramstart.web.dto.CMRespDto;
 
 @RestController //  응답리턴을 위해서 
@@ -14,8 +15,12 @@ import com.cos.photogramstart.web.dto.CMRespDto;
 public class ControllerExceptionHandler {
 
 	@ExceptionHandler(CustomValidationException.class)
-	public CMRespDto <?> validationException(CustomValidationException e) {
-		return new CMRespDto<Map<String,String>>(-1, e.getMessage(),  e.getErrorMap());
+	public String  validationException(CustomValidationException e) {
+		//CMRespDto, Scripte 비교
+		// 1. 클라이언트에게 응답할 때는 Script가 좋음 
+		// 2. Ajax 통신 - CMRespDto가 좋음
+		// 3. Android 통신 - CMRespDto가 좋음 
+		return Script.back(e.getErrorMap().toString());
 	}
 	
 }
