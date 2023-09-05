0fb92d5df73e1758c8384268b8b8e9ff10768936
0Zone <00zero0zone00@gmail.com>
2023-09-05 21:49:59 +0900

    스프링부트 포토그램 프로필 페이지 양방향 매핑 이해하기

--- photogram/src/main/java/com/cos/photogramstart/domain/user/User.java
+++ photogram/src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -1,19 +1,23 @@
 package com.cos.photogramstart.domain.user;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import com.cos.photogramstart.domain.image.Image;
 
 // JPA - Java Persistence API (자바로 데이터를 영구적으로 저장(DB)할 수 있는 API를 제공)
 
@@ -43,6 +47,13 @@ public class User {
 	
 	private String profileImageUrl;		//사진
 	private String role; 							//권한
+	
+	//나는 연관관계의 주인이 아니다.  그러므로 테이블에 컬럼을 만들지마라~ 
+	//User를 SELECT 할때 해당 User id로 등록된 image를 다 갖고와 
+	//LAZY 일때는 User를 SELECT 할때 해당 User id로 등록된 image들을 가져오지마 - 대신 getImages() 함수의 image들이  호츌될때 가져와 
+	//Eager 일때는 User를 SELECT 할때 해당 User id로 등록된 image들을 Join해서 가져와 ! 
+	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY) 
+	private List<Image> images; 		//양방향 맵핍 
 
 	private LocalDateTime createDate;
 	
