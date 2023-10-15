c52368c7b0cd5d4a7dcf6559db95b219810bc007
0Zone <00zero0zone00@gmail.com>
2023-10-15 18:46:55 +0900

    스프링부트 포토그램 - OAuth2 페이스북 로그인 (구현 완료)

--- photogram/src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ photogram/src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				//super 삭제 - 기존 시큐리티가 가지고 있는 기능이 다 비활성화됨.
 				http.csrf().disable();					//CSRF토큰을 비활성화
 				
-				System.out.println("11");
+				//System.out.println("11");
 				
 				http.authorizeRequests()
 					.antMatchers("/", "/user/**", "/image/**", "/subscribe/**" , "/comment/**", "/api/**").authenticated() 	//  / , /user , /image, /subscribe, /comment로 시작하는 주소로 들어오면 인증필요 
